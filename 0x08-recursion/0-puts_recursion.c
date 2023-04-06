38e6a8cf8a41bf1a6b7042bc21cec2bfeffa0d2a
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-04-06 07:11:39 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 /**
- * void _puts_recursion(char *s) - TAkes is char of value *s
+ * _puts_recursion - prints a string followed by a new line
+ *@s: string
  *
- * prints it out
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set 
  */
 void _puts_recursion(char *s)
 {
-	printf("%s/n", s);
+	if(*s == 0)
+	{
+		printf("/n");
+		return;
+	}
+	printf("%s", s);
+
+	_puts_recursion(s + 1);
 }
