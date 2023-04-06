faa8d0d345106b554f56da255b91af91532248a4
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-04-06 06:59:56 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- * void _puts_recursion(char *s)
+ * void _puts_recursion(char *s) - TAkes is char of value *s
+ *
+ * prints it out
  */
 void _puts_recursion(char *s)
 {
-	printf("%s", s);
+	printf("%s/n", s);
 }
