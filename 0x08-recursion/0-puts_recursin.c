29c373523bcfb09119bfa3efcd1bfac4c8a84088
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-06-07 14:27:55 +0200

    0. She locked away a secret, deep inside herself, sonething she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursin.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * puts recusion - input characters and print out the text
+ *
+ * Return - Always 0
+ */
+
+int _puts_recursion(char *s)
+{
+	int i;
+	char message[] = *s
+
+	for (i = 0; message[i] != '\0'; i++)
+	{
+		putchar(message[i]);
+	}
+}
