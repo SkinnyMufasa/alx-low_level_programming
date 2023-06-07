fc3fa62e740a53f96dabb100a6469cae22a48a27
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-06-07 16:49:36 +0200

    2. Dreams

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<strlen.h>
+/**
+ * _strlen_recursion - show the length of a string privided
+ *
+ * *s - String provided
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int length;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	length = 1 + _strlen_recursion(s + 1);
+	return (lenth);
+}
