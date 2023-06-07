fff61af2bf3668b06dec956bc8478c02f0f866ac
Kelly Mahlangu <kellymahlangu01@gmail.com>
2023-06-07 17:07:21 +0200

    3. You must not be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * factorial - returns the factorial of a given number
+ *
+ * n - Given number
+ *
+ * Return: Factorial of given number
+ */
+int factorial(int n)
+{
+	int fact;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	fact = factorial(n - 1) * n;
+	return (fact);	
+}
