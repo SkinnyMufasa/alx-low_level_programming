c5e45b8cb4f0663214aa55b1ec4da08d5457a294
SkinnyMufasa <113301610+SkinnyMufasa@users.noreply.github.com>
2023-06-09 00:16:13 +0200

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
  *
  * _pow_recursion - Function that returns the value of x raised to the power of y
  *
- * Return: x to the power y
+ * Return: x to the power y 
  */
 int _pow_recursion(int x, int y)
 {
