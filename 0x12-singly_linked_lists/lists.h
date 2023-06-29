4711e1cb038dbccb23edc30d7530a5d5bd1a0944
SkinnyMufasa <113301610+SkinnyMufasa@users.noreply.github.com>
2023-06-30 00:41:13 +0200

    header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-//Prototypes of all functions
+int _strlen(char *c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
