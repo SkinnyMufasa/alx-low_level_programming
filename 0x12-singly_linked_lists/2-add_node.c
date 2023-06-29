dfbea8cd3375f6db90082f170d7c4050a22e6e89
SkinnyMufasa <113301610+SkinnyMufasa@users.noreply.github.com>
2023-06-30 00:49:56 +0200

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,38 @@
 #include "lists.h"
 /**
- * add_node - Adds a new node at the beginning of a list_t list
+ * add_node - Adds a new node at the beginning of a 
+ * list_t list
  * @head: Pointer to the first list_t
  * @str: Pointer to the string of the new list_t
  * 
- * Return: The address of the new element, or NULL if it failed.
+ * Return: The address of the new element, or NULL 
+ * if it failed.
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    list_t *current;
+	char *dup;
+	int len;
+	list_t *new;
 
-    new = (list_t *)malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->str = (char *)malloc((_strlen(str) + 1) * sizeof(char));
-    new->str = strdup(str);
-    new->len = _strlen(new->str);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    if (*head == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        current = *head;
-        while (current->next != NULL)
-        {
-            current = current->next;
-        }
-        current->next = new;
-    }
-    return (new);
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+
 }
