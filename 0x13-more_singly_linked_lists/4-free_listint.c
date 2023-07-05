34053712fe47aea9ae4eb28238b01b4abeec4e3c
Kelly Mahlangu <kellymahlngu01@gmail.com>
2023-07-05 09:53:17 -0700

    please work

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -12,7 +12,7 @@ void free_listint(listint_t *head)
 	while (head)
 	{
 		temp = head->next;
-		free(head->n);
+		free((int *)(intptr_t)head->n);
 		free(head);
 		head = temp;
 	}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdarg.h>
 /**
 * struct listint_s - singly linked list
